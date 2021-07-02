46ac58f4895367cd2d31f31e778973682d23dcc7
Kevin Valdes <kevinvaldes3@gmail.com>
2021-07-02 03:44:45 -0500

    mensajes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'notFaound', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }, 
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
